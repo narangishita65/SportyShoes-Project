ebe1980b118cdcf250ba1a56059c87a3c3e240a0
narangishita65 <69043685+narangishita65@users.noreply.github.com>
2023-04-07 18:15:01 +0530

    Add files via upload

---
+++ SportyShoes-masterproject/SportyShoes.com/src/main/java/com/Ishita/phase3/CartDao.java
@@ -0,0 +1,17 @@
+package com.ishita.phase3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartDao {
+	
+	@Autowired
+	CartRepo c_repo;
+	
+	//USER INSERT ITEMS INTO CART'
+	public Cart addcart(Cart c)
+	{
+		return c_repo.save(c);
+	}
+}
